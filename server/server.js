4b9fc7a183b1828762e9cc6c263836cd701dbccd
Waleed Chaudhry <waleed.u.chaudhry@gmail.com>
2021-09-25 23:30:43 -0400

    Commit to add file storage

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ import Shopify, { ApiVersion } from "@shopify/shopify-api";
 import Koa from "koa";
 import next from "next";
 import Router from "koa-router";
+import fs from "fs";
 import routes from './router/index';
+import { Session } from "@shopify/shopify-api/dist/auth/session";
 
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
@@ -15,6 +17,31 @@ const app = next({
   dev,
 });
 const handle = app.getRequestHandler();
+const FILENAME = "./session.json";
+function storeCallback(session) {
+  console.log("storeCallback ");
+  fs.writeFileSync(FILENAME, JSON.stringify(session));
+  return true;
+}
+
+function loadCallback(id) {
+  console.log("loadCallback ");
+  if (fs.existsSync(FILENAME)) {
+    const sessionResult = fs.readFileSync(FILENAME, "utf8");
+    return Object.assign(new Session(), JSON.parse(sessionResult));
+  }
+  return false;
+}
+
+function deleteCallback(id) {
+  console.log("deleteCallback", id);
+}
+
+const sessionStorage = new Shopify.Session.CustomSessionStorage(
+  storeCallback,
+  loadCallback,
+  deleteCallback
+);
 
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
@@ -24,12 +51,17 @@ Shopify.Context.initialize({
   API_VERSION: ApiVersion.October20,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
-  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+  SESSION_STORAGE: sessionStorage,
 });
 
 // Storing the currently active shops in memory will force them to re-login when your server restarts. You should
 // persist this object in your app.
 const ACTIVE_SHOPIFY_SHOPS = {};
+const session = loadCallback();
+if (session?.shop && session?.scope) {
+  console.log("session", session);
+  ACTIVE_SHOPIFY_SHOPS[session.shop] = session.scope;
+}
 
 app.prepare().then(async () => {
   const server = new Koa();
